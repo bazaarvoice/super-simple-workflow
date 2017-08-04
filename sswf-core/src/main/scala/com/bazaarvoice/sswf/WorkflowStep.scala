dead83f2e39a4b94d0ffbd3422d882006329913a
vvcephei <john.roesler@bazaarvoice.com>
2017-08-04 09:56:13 -0500

    include in progress config in version. also include an override for version

--- sswf-core/src/main/scala/com/bazaarvoice/sswf/WorkflowStep.scala
+++ sswf-core/src/main/scala/com/bazaarvoice/sswf/WorkflowStep.scala
@@ -14,6 +14,13 @@ trait WorkflowStep {
 
   /** How long to wait before the next attempt when the step returns InProgress. */
   def inProgressSleepSecondsFn: InProgressSleepFunction
+
+  /** You shouldn't have to override this, but in case you make some change to a step that
+    * SSWF can't detect, you can bump this number up to tell SSWF to register a new version of the
+    * step.
+    * @return a single integer that forces a version update for step registration.
+    */
+  def versionOverride: Int = 0
 }
 
 trait InProgressSleepFunction {
